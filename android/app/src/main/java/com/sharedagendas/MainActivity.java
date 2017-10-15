42857338653a870ebefd04f402f34a299f40afba
Kévin Gossé <gossekevin1@gmail.com>
2017-10-15 02:48:25 +0200

    Initial commit

---
+++ android/app/src/main/java/com/sharedagendas/MainActivity.java
@@ -0,0 +1,7 @@
+package com.sharedagendas;
+
+import com.reactnativenavigation.controllers.SplashActivity;
+
+public class MainActivity extends SplashActivity {
+
+}
---
+++ ios/SharedAgendas/AppDelegate.m
@@ -0,0 +1,20 @@
+#import "AppDelegate.h"
+#import "RCCManager.h"
+#import <React/RCTRootView.h>
+#import <React/RCTBundleURLProvider.h>
+
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.backgroundColor = [UIColor whiteColor];
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation];
+  
+  return YES;
+}
+
+@end
